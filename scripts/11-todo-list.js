f62b6419c6a77a7a1371564ce581a56a47e069a0
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-07-20 15:33:20 -0600

    Add lesson 11.2

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,22 +1,46 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{ name: 'make dinner', dueDate: '2023-07-20'}, {name: 'wash dishes', dueDate: '2023-07-20'}];
 
 renderTodoList(todoList);
 
 function addTodo(){
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
+  const inputNameElement = document.querySelector('.js-name-input');
+  const name = inputNameElement.value;
 
-  todoList.push(name);
+  const inputDateElement = document.querySelector('.js-date-input');
+  const dueDate = inputDateElement.value;
 
-  inputElement.value = '';
+  const object = {
+    name,
+    dueDate
+  }
+
+  todoList.push(object);
+
+  inputNameElement.value = '';
   renderTodoList(todoList);
 }
 
 function renderTodoList(todoList){
   let todoListHTML = '';
+
   for(let i = 0; i < todoList.length; i++){
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    const {name , dueDate} = todoObject;
+    const html = `
+      <div>
+        ${name}
+      </div>
+      <div>
+        ${dueDate}
+      </div> 
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList(todoList);
+      "
+      class="delete-button">
+        Delete
+      </button>
+    `;
     todoListHTML += html;
   }
 
